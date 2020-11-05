d935bc9aaff3e5e3a387e7837eb2dcad773b8294
Stefanie Dias Costa <68706709+stefaniedias@users.noreply.github.com>
2020-11-05 14:50:57 -0300

    parte 2 do projeto integrador

---
+++ projetoIntegrador/ProjetoIntegradorPart2/ProjetoIntegrador/src/main/java/org/generation/deveducation/repository/TemaRepository.java
@@ -0,0 +1,8 @@
+package org.generation.deveducation.repository;
+
+import org.generation.deveducation.model.TemaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TemaRepository extends JpaRepository<TemaModel, Long> {
+
+}
